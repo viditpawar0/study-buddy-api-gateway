6d58fd9196fbfdb0be2b599979ce4f28d4870da7
Vidit Pawar <viditpawar0@gmail.com>
2025-09-20 01:52:08 +0530

    moved routing from ApiGatewayApplication.java to application.yml

--- src/main/java/com/studybuddy/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/studybuddy/api_gateway/ApiGatewayApplication.java
@@ -2,24 +2,11 @@ package com.studybuddy.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
-    }
-
-    @Bean
-    RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r -> r.path("/subject")
-                        .uri("lb://subject-trainer"))
-                .route(r -> r.path("/topic")
-                        .uri("lb://topic-trainer"))
-                .build();
     }
 }
